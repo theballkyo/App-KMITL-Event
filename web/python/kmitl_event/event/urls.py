94f43ab95c8818e84d509bb39d44fa82a0b57b6c
Theballkyo <theballkyo@gmail.com>
2016-10-03 23:26:15 +0700

    Add urls for event

---
+++ web/python/kmitl_event/event/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.home),
+    url(r'create/$', views.create),
+    
+    # Method GET=view, PUT=update, DELETE=delete
+    url(r'(?P<event_id>[0-9]+)$', views.do_event),
+]
--- web/python/kmitl_event/event/views.py
+++ web/python/kmitl_event/event/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+@login_required(redirect_field_name='')
+def home(request):
+    return HttpResponse("This is event/home")
+
+@login_required(redirect_field_name='')
+def create(request):
+    if request.POST:
+        # Create a event
+        pass
+    return render(request, "event/create.html")
+
+@login_required(redirect_field_name='')
+def do_event(request, event_id):
+    if request.PUT:
+        update_event(request, event_id)
+    elif request.DELETE:
+        delete_event(request, event_id)
+    else:
+        show_event(request, event_id)
+
+def show_event(request, event_id):
+    return HttpResponse("Event id: " + event_id)
+
+def update_event(request, event_id):
+    pass
+
+def delete_event(request, event_id):
+    pass
